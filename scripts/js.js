fb612d882e02c79aa8104656eedeffb40301bafb
onionmccabbage <onionmccabbage@live.com>
2021-11-15 15:03:38 +0000

    Add files via upload

---
+++ scripts/js.js
@@ -0,0 +1,30 @@
+// data models
+let model = [ // this is a JavaScript array of Objects
+    {item:'Pot', price:3.99}, 
+    {item:'Dot', price:2.99}, 
+    {item:'Hot', price:9.99}
+]
+// explore JSON tools
+let j = JSON.stringify(model) // converts anything to JSON (which is text)
+console.log(j, typeof(j))
+
+// event call-back functions
+const handleButton = () => {
+    // prevent the form from submitting!!
+    event.preventDefault()
+    // validate
+
+    // grab the user-values for category and num
+    let c = category.value
+    let n = num.value // same as n=document.getElementById('num').value
+    let apiUrl = 'https://jsonplaceholder.typicode.com/'
+    let l = `${apiUrl}${c}/${n}` // join the text together
+    // or l = `https://jsonplaceholder.typicode.com/${category.value}/${num.value}`
+
+    // use these values to make an http request for data
+    fetch(l) // defaults to a GET request (asynchronous)
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
+// event bindings
+btnGetData.addEventListener('click', handleButton)
